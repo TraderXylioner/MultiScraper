8eb35abd9bb1161898e8a8bd147d61945a93da2d
T_X <treider.xylioner@gmail.com>
2023-12-28 17:23:25 +0200

    add ServiceManager

---
+++ utils/__init__.py
@@ -0,0 +1,1 @@
+from utils.service import ServiceManager
--- utils/service.py
+++ utils/service.py
@@ -1,2 +1,12 @@
+from type import Service
+
+
 class ServiceManager:
-    ...
+    def __init__(self, services: list[Service] | None = None):
+        self.services = services if services else []
+
+    def add(self, service: Service):
+        self.services.append(service)
+
+    def __repr__(self):
+        return f'{self.__dict__}'
