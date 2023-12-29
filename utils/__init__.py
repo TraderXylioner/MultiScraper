5ad94a31df0dc723903d02c6406a1e6e82b35621
T_X <treider.xylioner@gmail.com>
2023-12-29 18:57:41 +0200

    add parser

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,3 @@
 from utils.service import ServiceManager
+from utils.proxy import ProxyManager
+from utils.parser import Parser
--- utils/parser.py
+++ utils/parser.py
@@ -1,2 +1,35 @@
+import asyncio
+
+from utils import ProxyManager, ServiceManager
+
+
 class Parser:
-    ...
+    def __init__(self,
+                 proxies: ProxyManager | None = None,
+                 services: ServiceManager | None = None):
+        self.proxies = proxies if proxies else ProxyManager()
+        self.services = services if services else ServiceManager()
+        self.work = {}
+
+    def __repr__(self):
+        return f'proxies: {self.proxies}, ' \
+               f'services: {self.services}'
+
+    def check_data(self):
+        self.proxies._check_data()
+        self.services._check_data()
+
+    def create_work(self):
+        for proxy in self.proxies:
+            self.work[proxy.ip] = {service.name: service.rate_limit for service in self.services}
+
+    async def limit_update(self):
+        while True:
+            await asyncio.sleep(1)
+            self.create_work()
+            print(self.work)
+
+    async def start(self):
+        self.check_data()
+        self.create_work()
+        asyncio.create_task(self.limit_update())
