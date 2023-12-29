20ffae454d1a4ad62679ed625ee136d6f0a4dd6e
T_X <treider.xylioner@gmail.com>
2023-12-29 18:57:07 +0200

    refactor service

--- utils/service.py
+++ utils/service.py
@@ -8,5 +8,24 @@ class ServiceManager:
     def add(self, service: Service):
         self.services.append(service)
 
+    def remove(self, service: Service):
+        self.services.remove(service)
+
     def __repr__(self):
-        return f'{self.__dict__}'
+        return f'{self.services}'
+
+    def __len__(self):
+        return len(self.services)
+
+    def __getitem__(self, index):
+        return self.services[index]
+
+    def __setitem__(self, index, value):
+        self.services[index] = value
+
+    def __delitem__(self, index):
+        del self.services[index]
+
+    def _check_data(self):
+        if not self.services:
+            raise ValueError('Service not specified')
