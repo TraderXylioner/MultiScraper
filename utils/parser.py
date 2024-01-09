489edeb83679aa9310f4a5da3e65c915ec3b2286
T_X <treider.xylioner@gmail.com>
2024-01-09 14:03:18 +0200

    refactor parser

--- utils/parser.py
+++ utils/parser.py
@@ -2,73 +2,30 @@ import asyncio
 
 import aiohttp
 
-from type import Proxy, Task, ProxyManager, ServiceManager
+from type import Request, Task
+from utils.manager import Manager
 
 
-# decomposition
 class Parser:
-    def __init__(self,
-                 proxies: ProxyManager | None = None,
-                 services: ServiceManager | None = None):
-        self.proxies = proxies if proxies else ProxyManager()
-        self.services = services if services else ServiceManager()
-        self.work = {}
+    def __init__(self, manager: Manager):
+        self.manager = manager
 
-    def __repr__(self):
-        return f'proxies: {self.proxies}, ' \
-               f'services: {self.services}'
-
-    def _check_data(self):
-        self.proxies.check_data()
-        self.services.check_data()
-
-    def create_work(self):
-        for proxy in self.proxies:
-            self.work[proxy] = {service.name: service.rate_limit for service in self.services}
-
-    async def updating_work(self):
-        while True:
-            await asyncio.sleep(1)
-            self.create_work()
-
-    async def run_tasks(self, tasks: list[Task]):
-        tasks = [asyncio.create_task(self.send_request(task)) for task in tasks]
-        for task in tasks:
+    async def run_task(self, task: Task):
+        task = [asyncio.create_task(self.send_request(req)) for req in task]
+        for req in task:
             # await self.send_request(task)
-            await task
+            await req
         return task
 
-    async def run_manager(self):
-        self._check_data()
-        self.create_work()
-        asyncio.create_task(self.updating_work())
-
-    async def get_proxy(self, task: Task):
-        while True:
-            base_limit = 0
-            res = None
-            for proxy in self.work:
-                limit = self.work[proxy].get(task.service)
-                if limit is None:
-                    raise ValueError(f'service "{task.service}" not found')
-                if limit > base_limit:
-                    base_limit = limit
-                    res = proxy
-            if base_limit > 0:
-                self.work[res][task.service] -= 1
-                task.proxy = res
-                break
-            else:
-                await asyncio.sleep(0.1)
-
-    async def send_request(self, task: Task):
+    async def send_request(self, request: Request):
         attempts = 0
         while attempts < 10:
             try:
-                await self.get_proxy(task)
+                await self.manager.get_proxy(request)
                 async with aiohttp.ClientSession(trust_env=True) as session:
-                    async with session.get(url=task.url, proxy=task.proxy.to_string(), ssl=True) as response:
-                        task.response = await response.json()
+                    async with session.get(url=request.url, proxy=request.proxy.to_string(), ssl=True) as response:
+                        request.response = await response.json()
+                        print(request.response)
                         break
             except Exception as ex:
                 raise ex
