d0f95522db3e963f8100546825a5ab14f8f05eb4
T_X <treider.xylioner@gmail.com>
2024-02-05 18:16:04 +0200

    protected response, proxy and add property

--- type/request.py
+++ type/request.py
@@ -2,5 +2,13 @@ class Request:
     def __init__(self, url: str, service: str):
         self.url = url
         self.service = service
-        self.response = None
-        self.proxy = None
+        self._response = None
+        self._proxy = None
+
+    @property
+    def response(self):
+        return self._response
+
+    @response.setter
+    def response(self, response):
+        self._response = response
