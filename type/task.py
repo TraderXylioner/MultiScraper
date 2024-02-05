58d33cd8658ec66b37c43d8fd7897aaa8e3ddfd0
T_X <treider.xylioner@gmail.com>
2024-02-05 18:15:20 +0200

    task -> _tasks

--- type/task.py
+++ type/task.py
@@ -6,25 +6,25 @@ from type import Request
 @dataclass
 class Task:
     def __init__(self, task: list[Request] | None = None):
-        self.task = task if task else []
+        self._tasks = task if task else []
 
     def add(self, task: Request):
-        self.task.append(task)
+        self._tasks.append(task)
 
     def remove(self, task: Request):
-        self.task.remove(task)
+        self._tasks.remove(task)
 
     def __repr__(self):
-        return f'{self.task}'
+        return f'{self._tasks}'
 
     def __len__(self):
-        return len(self.task)
+        return len(self._tasks)
 
     def __getitem__(self, index):
-        return self.task[index]
+        return self._tasks[index]
 
     def __setitem__(self, index, value):
-        self.task[index] = value
+        self._tasks[index] = value
 
     def __delitem__(self, index):
-        del self.task[index]
+        del self._tasks[index]
